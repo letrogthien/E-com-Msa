2fa8858f073ddcd10d0255be62c1a0efe22a12bf
letrogthien <letrogthien@gmail.com>
2025-07-13 20:39:00 +0700

    #1 - letrogthien update rsa for jwt

--- auth/src/main/java/com/letrogthien/auth/services/AuthService.java
+++ auth/src/main/java/com/letrogthien/auth/services/AuthService.java
@@ -1,21 +1,25 @@
 package com.letrogthien.auth.services;
 
+
+import com.letrogthien.auth.common.RoleName;
 import com.letrogthien.auth.requests.*;
 import com.letrogthien.auth.responses.ApiResponse;
 import com.letrogthien.auth.responses.LoginResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.UUID;
 
 public interface AuthService {
     ApiResponse<String> register(RegisterRequest registerRequest);
 
-    ApiResponse<LoginResponse> login(LoginRequest loginRequest);
+    ApiResponse<LoginResponse> login(LoginRequest loginRequest, HttpServletResponse response);
 
     ApiResponse<String> logout(LogoutRequest logoutRequest);
 
     ApiResponse<String> logoutAll(UUID userId);
 
     ApiResponse<LoginResponse> accessToken(AccessTokenRequest accessTokenRequest);
+
 
     ApiResponse<String> changePassword(ChangePwdRequest changePwdRequest, UUID userId);
 
@@ -25,11 +29,14 @@ public interface AuthService {
 
     ApiResponse<String> disableTwoFAuth(Disable2FaRequest disable2FAuthRequest, UUID userId);
 
-    ApiResponse<LoginResponse> verifyTwoFAuth(Verify2FaRequest verify2FaRequest);
+    ApiResponse<LoginResponse> verifyTwoFAuth(Verify2FaRequest verify2FaRequest, HttpServletResponse response);
+
 
     ApiResponse<String> trustDevice(String deviceName, String deviceType, UUID userId);
 
     ApiResponse<String> activateAccount(String token);
 
-    ApiResponse<String> resetPassword(ResetPwdRequest resetPwdRequest);
+    ApiResponse<String> resetPassword(ResetPwdRequest resetPwdRequest, String token);
+
+    ApiResponse<String> assignRoleToUser(UUID userId, RoleName roleName);
 }
